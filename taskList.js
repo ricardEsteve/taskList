7265d08b99749802317b291964b5cd0a0fc36200
Ricard <ricard.esteve2405@outook.com>
2017-10-25 12:28:22 +0200

    adding firebase

--- taskList.js
+++ taskList.js
@@ -1,59 +1,154 @@
-(function () {
-  //  variables
-  var list = document.getElementById("list"),
-      taskInput = document.getElementById("taskInput"),
-      btnNewTask = document.getElementById("addButton");
+$(function () {
 
-  //  functions
-  var addThisTask = function () {
-    var task = taskInput.value,
-        newTask = document.createElement("li"),
-      link = document.createElement("a");
-    content = document.createTextNode(task);
+  var $loginButton = $("#loginButton");
+  var $logOutButton = $("#logOutButton");
+  var list = document.getElementById("list");
+  var taskInput = document.getElementById("taskInput");
+  var btnNewTask = document.getElementById("addButton");
+  var provider = new firebase.auth.GoogleAuthProvider();
+  provider.addScope('https://www.googleapis.com/auth/plus.login');
+  firebase.auth().signInWithRedirect(provider);
+  
+
+
+  document.getElementById("addButton").addEventListener("click", addThisTask);
+
+  function addThisTask(uid, body) {
+
+    var postData = {
+      uid: uid,
+      body: body,
+    };
+
+
+    var task = taskInput.value;
+    var newTask = document.createElement("li");
+    var link = document.createElement("a");
+    var content = document.createTextNode(task);
+
+//    firebase.auth().onAuthStateChanged(function (user) {
+//      if (user != null) {
+//        $("#e-mail").val("");
+//        $("#password").val("");
+//        $("#panelSignIn").hide();
+//        $("#logOutButton").show();
+//
+//      } else {
+//        $("#panelSignIn").show();
+//        $("#logOutButton").hide();
+//      }
+//    });
+
+//    $loginButton.on('click', function () {
+//      var $email = $("#e-mail");
+//      var $password = $("#password");
+//
+//      firebase.auth().signInWithEmailAndPassword($email.val(), $password.val());
+//    });
+
+
+    $logOutButton.on('click', function () {
+      firebase.auth().signOut();
+
+    });
+
+
+
+    var verifyInput = function () {
+      taskInput.className = "";
+      taskInput.setAttribute("placeholder", "add your task");
+    };
+    var deleteTask = function () {
+      this.parentNode.removeChild(this);
+    };
+
 
     if (task == "") {
       taskInput.setAttribute("placeholder", "add a real task");
       taskInput.className = "error";
       return false;
     }
-    //    time to add the content to the link
+
     link.appendChild(content);
-    //we create an attribe href to be able to click on the task
     link.setAttribute("href", "#");
-    //time to add the link to the new task (which is a "li")
     newTask.appendChild(link);
 
     list.appendChild(newTask);
 
     taskInput.value = "";
 
+    btnNewTask.addEventListener("click", addThisTask);
+
+    taskInput.addEventListener("click", verifyInput);
+
+
     for (var i = 0; i <= list.children.length - 1; i++) {
       list.children[i].addEventListener("click", function () {
 
         this.parentNode.removeChild(this);
-      })
+      });
 
     }
-  };
+    
+    getCurrentUserUid();
 
-  var verifyInput = function () {
-    taskInput.className = "";
-    taskInput.setAttribute("placeholder", "add your task");
-  };
-  var deleteTask = function () {
-    this.parentNode.removeChild(this);
-  };
+    var newPostKey = firebase.database().ref().child('posts').push().key;
+    var updates = {};
+    updates['/posts/' + newPostKey] = postData;
+    return firebase.database().ref().update(updates);
 
-  //  events
+    function getCurrentUserUid() {
+      return firebase.auth().currentUser.uid;
 
-  //  this function adds a task
-  btnNewTask.addEventListener("click", addThisTask);
+    }
 
-  //  verify the input
-  taskInput.addEventListener("click", verifyInput);
-  //to delete the input once the task is done
-  for (var i = 0; i <= list.children.length - 1; i++) {
-    list.children[i].addEventListener("click", deleteTask);
+
+    for (var i = 0; i <= list.children.length - 1; i++) {
+      list.children[i].addEventListener("click", deleteTask);
+    }
+
   }
 
-}());
+});
+
+    
+
+
+
+//document.getElementById("login").addEventListener("click", login);
+//document.getElementById("create-post").addEventListener("click", function() {
+//    writeNewPost(getCurrentUserUid(), "yujuu");
+//});
+//
+//function writeNewPost(uid, body) {
+//  // A post entry.
+//  var postData = {
+//    uid: uid,
+//    body: body,
+//  };
+//
+//  // Get a key for a new Post.
+//  var newPostKey = firebase.database().ref().child('posts').push().key;
+//  
+//  var updates = {};
+//  updates['/posts/' + newPostKey] = postData;
+//
+//  return firebase.database().ref().update(updates);
+//}
+//
+//function getCurrentUserUid() {
+//    return firebase.auth().currentUser.uid;
+//}
+//
+//function login() {
+//    var provider = new firebase.auth.GoogleAuthProvider();
+//    firebase.auth().signInWithPopup(provider);
+//}
+//
+//function getPosts() {
+//    
+// firebase.database().ref('/posts/').on('value').then(function(snapshot) {
+//    console.log(snapshot.val());
+//  });
+//
+//}
